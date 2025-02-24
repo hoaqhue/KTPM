ab64d8e52a37ea9f69152b68fff2b52d80ba0312
you@example.com <hoanghue16804@gmail.com>
2025-02-24 14:07:04 +0700

    update feature test

--- UnitTestDemo/src/main/java/com/mycompany/unittestdemo/UnitTestDemo.java
+++ UnitTestDemo/src/main/java/com/mycompany/unittestdemo/UnitTestDemo.java
@@ -12,6 +12,6 @@ public class UnitTestDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World 2!");
+        System.out.println("Hello 05 World 2!");
     }
 }
